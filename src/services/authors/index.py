408a0329e9f68d2573fae9f27cb26c5b6e92dbe5
baketti <e.bachetti1997@gmail.com>
2024-09-14 19:47:53 +0200

    Added get_or_create_author,get_author_by_authorId and get_author_by_authorName functions

--- src/services/authors/index.py
+++ src/services/authors/index.py
@@ -15,3 +15,35 @@ def post_author(conn, author_data):
     except Exception as e:
         conn.rollback()
         print(f"An error occurred during author creation: {e}")
+
+def get_or_create_author(conn, author_name):
+    print('get_or_create_author->',author_name)
+    try:
+        author = get_author_by_authorName(conn, author_name)
+        if not author:
+            return post_author(conn, {
+                "name": author_name,
+                "birth_date": None,
+                "email": None
+            })
+        return author
+    except Exception as e:
+        conn.rollback()
+        print(f"An error occurred while getting or creating author: {e}")
+        return None
+
+def get_author_by_authorId(conn, author_id):
+    try:
+        author = conn.query(Author).filter(Author.id == author_id).first()
+        return author
+    except Exception as e:
+        print(f"An error occurred during author retrieval: {e}")
+        return None
+
+def get_author_by_authorName(conn, author_name):
+    try:
+        author = conn.query(Author).filter(Author.name == author_name).first()
+        return author
+    except Exception as e:
+        print(f"An error occurred during author retrieval: {e}")
+        return None
