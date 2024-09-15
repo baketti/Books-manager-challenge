1312465989f2a984c4f07e6143288dc43a356b32
baketti <e.bachetti1997@gmail.com>
2024-09-15 16:49:11 +0200

    CLI menu for book operations created

--- src/cli/operations/index.py
+++ src/cli/operations/index.py
@@ -1,5 +1,7 @@
 from cli.menu.authors.index import authors_menu
+from cli.menu.books.index import books_menu
 from cli.operations.authors.index import post_author_from_CLI,get_all_authors_from_CLI,get_author_by_authorId_from_CLI,put_author_by_authorId_from_CLI,delete_author_by_authorId_from_CLI
+from cli.operations.books.index import post_book_from_CLI,get_all_books_from_CLI,get_book_by_bookId_from_CLI,put_book_by_bookId_from_CLI,delete_book_by_bookId_from_CLI
 
 def authors_operations(connection):
     while True:
@@ -14,6 +16,24 @@ def authors_operations(connection):
             put_author_by_authorId_from_CLI(connection)
         elif choice == '5':
             delete_author_by_authorId_from_CLI(connection)
+        elif choice == '6':
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+def books_operations(connection):
+    while True:
+        choice = books_menu()
+        if choice == '1':
+            post_book_from_CLI(connection)
+        elif choice == '2':
+            get_all_books_from_CLI(connection)
+        elif choice == '3':
+            get_book_by_bookId_from_CLI(connection)
+        elif choice == '4':
+            put_book_by_bookId_from_CLI(connection)
+        elif choice == '5':
+            delete_book_by_bookId_from_CLI(connection)
         elif choice == '6':
             break
         else:
