58bc2ee37dc64c9fb439b19a20e260b9d7db28f0
baketti <e.bachetti1997@gmail.com>
2024-09-15 13:05:05 +0200

    Books CLI menu created

---
+++ src/cli/menu/books/index.py
@@ -0,0 +1,9 @@
+def books_menu():
+    print("\n--- Gestione Libri ---")
+    print("1. Aggiungi un libro")
+    print("2. Visualizza tutti i libri")
+    print("3. Visualizza un libro specifico")
+    print("4. Aggiorna un libro")
+    print("5. Elimina un libro")
+    print("6. Torna al menù principale")
+    return input("Seleziona un'opzione: ")
