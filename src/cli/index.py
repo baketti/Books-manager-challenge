fcf61bae02dbe426de398a3b207b094706dab1a8
baketti <e.bachetti1997@gmail.com>
2024-09-15 13:02:06 +0200

    Main CLI menu created

---
+++ src/cli/index.py
@@ -0,0 +1,6 @@
+def main_menu():
+    print("\n--- Gestione Biblioteca ---")
+    print("1. Gestione Libri")
+    print("2. Gestione Autori")
+    print("3. Esci")
+    return input("Seleziona un'opzione: ")
