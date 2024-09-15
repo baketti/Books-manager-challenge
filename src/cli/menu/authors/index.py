1ce0c958acbece23ad3fd4ad55ca6770c06b4ab3
baketti <e.bachetti1997@gmail.com>
2024-09-15 13:05:56 +0200

    Authors CLI menu created

---
+++ src/cli/menu/authors/index.py
@@ -0,0 +1,9 @@
+def authors_menu():
+    print("\n--- Gestione Autori ---")
+    print("1. Aggiungi un autore")
+    print("2. Visualizza tutti gli autori")
+    print("3. Visualizza un autore specifico")
+    print("4. Aggiorna un autore")
+    print("5. Elimina un autore")
+    print("6. Torna al menù principale")
+    return input("Seleziona un'opzione: ")
