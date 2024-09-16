79585b221d5f4be947264818a7a97cda0ebce94e
baketti <e.bachetti1997@gmail.com>
2024-09-16 16:59:02 +0200

    Refactor get_validated_input to use Prompt

--- src/cli/inputs/index.py
+++ src/cli/inputs/index.py
@@ -1,6 +1,15 @@
-def get_validated_input(prompt: str, validation_func, is_required: bool=False):
+from rich.prompt import Prompt, IntPrompt, FloatPrompt
+
+def get_validated_input(prompt: str, validation_func, field_type=str, is_required: bool=False):
     while True:
-        value = input(prompt)
+        value = None
+        if field_type == int: 
+            value = IntPrompt.ask(prompt)
+        elif field_type == float: 
+            value = FloatPrompt.ask(prompt)
+        else: 
+            value = Prompt.ask(prompt)
+
         if not value and not is_required:
             return None
         try:
