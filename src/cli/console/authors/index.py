20d7308ef2ecc89b9451fb95ab527718a944881d
baketti <e.bachetti1997@gmail.com>
2024-09-16 12:38:46 +0200

    Fix print_table params order

--- src/cli/console/authors/index.py
+++ src/cli/console/authors/index.py
@@ -4,4 +4,4 @@ from utils.functions.index import convert_to_dict_list
 def print_authors_data(authors):
     colums = ["ID", "Name", "Birth date", "Email"]
     authors_data = convert_to_dict_list(authors)
-    print_table(colums, authors_data, "Authors")
+    print_table(authors_data, colums, "Authors")
