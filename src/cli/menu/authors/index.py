1ffeca467a27e7496cda038fbf1e50c338d69db4
baketti <e.bachetti1997@gmail.com>
2024-09-16 15:26:37 +0200

    Add prompt with choices to select an option

--- src/cli/menu/authors/index.py
+++ src/cli/menu/authors/index.py
@@ -1,4 +1,5 @@
 from cli.console.index import print_title
+from rich.prompt import Prompt
 
 def authors_menu():
     print_title("AUTHOR MANAGEMENT")
@@ -8,4 +9,4 @@ def authors_menu():
     print("4. Update an author")
     print("5. Delete an author")
     print("6. Return to main menu")
-    return input("Select an option: ")
+    return Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6"])
