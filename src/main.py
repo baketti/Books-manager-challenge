69a8966f703e13acf1813a1ddb9f608b7ee05c2a
baketti <e.bachetti1997@gmail.com>
2024-09-16 21:24:43 +0200

    Fix  KeyboardInterrupt interrupt exception triggered by CTRL+C

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+from cli.console.index import print_exit_message
 from db.index import create_session
 from cli.index import display_CLI_menu
 
 def main():
-    connection = create_session()
-    display_CLI_menu(connection)    
+    try:
+        connection = create_session()
+        display_CLI_menu(connection)  
+    except KeyboardInterrupt:
+        print_exit_message()
+        exit(0)  
        
 if __name__ == '__main__':
     main()
