8d06093c79d94595cf81de61e696cbfaa7311a46
baketti <e.bachetti1997@gmail.com>
2024-09-16 15:16:00 +0200

    Add prompt with choices

--- src/cli/menu/index.py
+++ src/cli/menu/index.py
@@ -1,8 +1,9 @@
 from cli.console.index import print_title
+from rich.prompt import Prompt, Confirm
 
 def main_menu():
     print_title("WELCOME TO THE LIBRARY MANAGEMENT SYSTEM")
     print("1. Book Management")
     print("2. Author Management")
     print("3. Exit")
-    return input("Select an option: ")
+    return Prompt.ask("Select an option", choices=["1", "2", "3"])
