c3094d00fefc475546e549087f3d3f2be64de02a
baketti <e.bachetti1997@gmail.com>
2024-09-16 14:29:07 +0200

    Add custom success and error logs

--- src/db/init/index.py
+++ src/db/init/index.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from cli.console.index import print_success,print_error
 import logging
 
 Base = declarative_base()
@@ -9,11 +10,11 @@ def init_struct():
     engine = None
     try:
         engine = create_engine('sqlite:///db/books_authors.db', echo=True)
-        print("Database 'books_authors' created successfully!")
+        print_success("Database 'books_authors' created successfully!")
         Base.metadata.create_all(engine)
-        print("'books' and 'authors' tables created successfully!")
+        print_success("'books' and 'authors' tables created successfully!")
     except Exception as e:
-        print(f"An error was occurred during database structure initialization: {e}")
+        print_error(f"An error was occurred during database structure initialization: {e}")
     return engine
 
 def disable_sqlalchemy_logging():
