46b5879cd86a6fb27379b93743426cfff4cdc767
baketti <e.bachetti1997@gmail.com>
2024-09-16 19:49:03 +0200

    Restore get_validated_input and solved the issue

--- src/cli/inputs/index.py
+++ src/cli/inputs/index.py
@@ -1,15 +1,6 @@
-from rich.prompt import Prompt, IntPrompt, FloatPrompt
-
-def get_validated_input(prompt: str, validation_func, field_type=str, is_required: bool=False):
+def get_validated_input(prompt: str, validation_func, is_required: bool=False):
     while True:
-        value = None
-        if field_type == int: 
-            value = IntPrompt.ask(prompt)
-        elif field_type == float: 
-            value = FloatPrompt.ask(prompt)
-        else: 
-            value = Prompt.ask(prompt)
-
+        value = input(f"{prompt}: ")
         if not value and not is_required:
             return None
         try:
