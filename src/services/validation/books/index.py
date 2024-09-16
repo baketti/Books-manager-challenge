bb4a2b33483cb1a2580f52590f7eecaa99d5c461
baketti <e.bachetti1997@gmail.com>
2024-09-16 19:41:10 +0200

    Reset num and float validation

--- src/services/validation/books/index.py
+++ src/services/validation/books/index.py
@@ -1,3 +1,5 @@
+from utils.functions.index import convert_to_number
+
 def validate_title(title: str):
     if not title:
         raise ValueError("title is required")
@@ -5,26 +7,28 @@ def validate_title(title: str):
         raise ValueError("title must be at most 255 characters long")
     return title
 
-def validate_pages(pages: int):
+def validate_pages(pages: str):
     try:
-        if pages < 1:
+        _pages = convert_to_number(pages,'pages',int)
+        if _pages < 1:
             raise ValueError("pages must be a at least 1")
-        elif pages > 10000:
+        elif _pages > 10000:
             raise ValueError("Pages must be at most 10000")
-        return pages
+        return _pages
     except ValueError as e:
         raise ValueError(e)
 
-def validate_price(price: float):
+def validate_price(price: str):
     try:
-        if price < 0:
+        _price = convert_to_number(price,'price',float)
+        if _price < 0:
             raise ValueError("price must be at least 0.01$")
-        elif price > 10000.00:
+        elif _price > 10000.00:
             raise ValueError("Price must be at most 10000.00$")
-        return price
+        return _price
     except ValueError as e:
         raise ValueError(e)
-
+    
 def validate_publisher(publisher: str):
     if len(publisher) > 255:
         raise ValueError("Publisher must be at most 255 characters long")
