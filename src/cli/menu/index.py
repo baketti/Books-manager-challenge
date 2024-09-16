6d52dfb783968cadcbfbc1d3fb2ec8d9dc039a42
baketti <e.bachetti1997@gmail.com>
2024-09-16 14:26:47 +0200

    Add custom title print

--- src/cli/menu/index.py
+++ src/cli/menu/index.py
@@ -1,5 +1,7 @@
+from cli.console.index import print_title
+
 def main_menu():
-    print("\n--- Library Management ---")
+    print_title("WELCOME TO THE LIBRARY MANAGEMENT SYSTEM")
     print("1. Book Management")
     print("2. Author Management")
     print("3. Exit")
