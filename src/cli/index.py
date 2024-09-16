0bc3006027c355f32555ba8a52e8962934070797
baketti <e.bachetti1997@gmail.com>
2024-09-16 15:07:10 +0200

    Add confirm to exit the program

--- src/cli/index.py
+++ src/cli/index.py
@@ -1,5 +1,6 @@
 from cli.menu.index import main_menu
 from cli.operations.index import books_operations, authors_operations
+from rich.prompt import Confirm
 
 def display_CLI_menu(connection):
     while True:
@@ -9,8 +10,10 @@ def display_CLI_menu(connection):
         elif choice == '2':
             authors_operations(connection)
         elif choice == '3':
-            print("Exiting the program.")
-            connection.close()
-            break
+            exit = Confirm.ask("Are you sure you want to exit the program?")
+            if exit:
+                print("Exiting the program.")
+                connection.close()
+                break
         else:
             print("Invalid choice, please try again.")
