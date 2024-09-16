5bf644d7692df0d25c48ff605c3f2b564b5f2ede
baketti <e.bachetti1997@gmail.com>
2024-09-16 19:45:07 +0200

    Add custom title param to print with data

--- src/cli/console/authors/index.py
+++ src/cli/console/authors/index.py
@@ -1,7 +1,7 @@
 from cli.console.index import print_table
 from utils.functions.index import convert_to_dict_list
 
-def print_authors_data(authors):
+def print_authors_data(authors, title="Authors"):
     colums = ["ID", "Name", "Birth date", "Email"]
     authors_data = convert_to_dict_list(authors)
-    print_table(authors_data, colums, "Authors")
+    print_table(authors_data, colums, title)
