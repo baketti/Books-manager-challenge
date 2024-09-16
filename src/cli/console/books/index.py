0ac498dedc027ff3e827ff9bc1a1bd66bf02b498
baketti <e.bachetti1997@gmail.com>
2024-09-16 12:38:06 +0200

    Fix print_table params order

--- src/cli/console/books/index.py
+++ src/cli/console/books/index.py
@@ -4,4 +4,4 @@ from utils.functions.index import convert_to_dict_list
 def print_books_data(books):
     colums = ["ID", "Title", "Author ID", "Pages", "Price", "Publisher"]
     books_data = convert_to_dict_list(books)
-    print_table(colums, books_data, "Books")
+    print_table(books_data, colums, "Books")
