091c089a1babc33f5d216ce067da3d7dbcf8fbfd
baketti <e.bachetti1997@gmail.com>
2024-09-16 19:40:29 +0200

    Reset convert_to_number function

--- src/utils/functions/index.py
+++ src/utils/functions/index.py
@@ -1,2 +1,13 @@
+def convert_to_number(value,field_name,field_type):
+    if not value or value.strip() == '':
+        return None 
+    try:
+        return field_type(value)
+    except ValueError:
+        raise ValueError(f"{field_name} must be a number")
+
 def convert_to_dict_list(elements):
     return [element.to_dict() for element in elements]
+
+def is_updated(updated_data):
+    return any(updated_data[key] != None for key in updated_data.keys())
