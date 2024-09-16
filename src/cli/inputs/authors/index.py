e4c734dc29f7247a7d2b65d06cb2c1594109f58e
baketti <e.bachetti1997@gmail.com>
2024-09-16 18:20:16 +0200

    Fixed input labels

--- src/cli/inputs/authors/index.py
+++ src/cli/inputs/authors/index.py
@@ -2,9 +2,9 @@ from services.validation.authors.index import validate_author_name, validate_aut
 from cli.inputs.index import get_validated_input
 
 def get_POST_author_input_data():
-    name = get_validated_input("Name", validate_author_name, is_required=True)
-    birth_date = get_validated_input("Birth date (YYYY/MM/DD)", validate_author_birth_date)
-    email = get_validated_input("Email", validate_author_email)
+    name = get_validated_input("Name (required)", validate_author_name, is_required=True)
+    birth_date = get_validated_input("Birth date (YYYY/MM/DD) (optional)", validate_author_birth_date)
+    email = get_validated_input("Email (optional)", validate_author_email)
     return {
         "name": name,
         "birth_date": birth_date,
--- src/cli/inputs/books/index.py
+++ src/cli/inputs/books/index.py
@@ -3,8 +3,8 @@ from services.validation.books.index import validate_title, validate_pages, vali
 from services.validation.authors.index import validate_author_name
 
 def get_POST_book_input_data():
-    title = get_validated_input("Title", validate_title, is_required=True)
-    author_name = get_validated_input("Author Name", validate_author_name, is_required=True)
+    title = get_validated_input("Title (required)", validate_title, is_required=True)
+    author_name = get_validated_input("Author Name (required)", validate_author_name, is_required=True)
     pages = get_validated_input("Number of pages (optional)", validate_pages, field_type=int)
     price = get_validated_input("Cover price (optional)", validate_price, field_type=float)
     publisher = get_validated_input("Publisher (optional)", validate_publisher)
@@ -19,9 +19,9 @@ def get_POST_book_input_data():
 def get_PUT_book_input_data():
     title = get_validated_input("New title", validate_title)
     author_name = get_validated_input("New author", validate_author_name)
-    pages = get_validated_input("New number of pages (optional)", validate_pages)
-    price = get_validated_input("New cover price (optional)", validate_price)
-    publisher = get_validated_input("New publisher (optional)", validate_publisher)
+    pages = get_validated_input("New number of pages", validate_pages)
+    price = get_validated_input("New cover price", validate_price)
+    publisher = get_validated_input("New publisher", validate_publisher)
     return {
         "title": title,
         "author_name": author_name,
