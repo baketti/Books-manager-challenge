82ae559b82d25c5e21fa2d2684a8b76ff648b96a
baketti <e.bachetti1997@gmail.com>
2024-09-16 14:38:58 +0200

    Add custom warning and error logs

--- src/cli/operations/authors/index.py
+++ src/cli/operations/authors/index.py
@@ -1,47 +1,45 @@
 from services.authors.index import delete_author_by_authorId, get_all_authors, get_author_by_authorId, post_author, put_author_by_authorId
 from cli.inputs.authors.index import get_POST_author_input_data, get_PUT_author_input_data
 from cli.console.authors.index import print_authors_data
+from cli.console.index import print_error, print_warning
 
 def post_author_from_CLI(connection):
     author_data = get_POST_author_input_data()
     post_author(connection, author_data)
 
 def get_all_authors_from_CLI(connection):
-    print("List of all authors:")
     authors = get_all_authors(connection)
     if authors: print_authors_data(authors)
-    else:
-        print("No authors found.")
+    else: print_warning("No authors found.")
 
 def get_author_by_authorId_from_CLI(connection):
     try:
         author_id = int(input("Enter the author ID: "))
         author = get_author_by_authorId(connection, author_id)
         if author: print_authors_data([author])
-        else:
-            print("Author not found.")
+        else: print_warning("Author not found.")
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
 
 def put_author_by_authorId_from_CLI(connection):
     try:
         author_id = int(input("Enter the ID of the author to update: "))
         author = get_author_by_authorId(connection, author_id)
         if not author:
-            print("Cannot update it, this author does not exist!\nRepeat the operation and enter an existing ID.")
+            print_warning("Cannot update it, this author does not exist!\nRepeat the operation and enter an existing ID.")
             return
         updated_data = get_PUT_author_input_data()
         put_author_by_authorId(connection, author_id, updated_data)
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
 
 def delete_author_by_authorId_from_CLI(connection):
     try:
         author_id = int(input("Enter the ID of the author to delete: "))
         author = get_author_by_authorId(connection, author_id)
         if not author:
-            print("Cannot delete it, this author does not exist!\nRepeat the operation and enter an existing ID.")
+            print_warning("Cannot delete it, this author does not exist!\nRepeat the operation and enter an existing ID.")
             return
         delete_author_by_authorId(connection, author_id)
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
--- src/cli/operations/books/index.py
+++ src/cli/operations/books/index.py
@@ -1,47 +1,45 @@
 from services.books.index import get_book_by_bookId, post_book, get_all_books, put_book_by_bookId, delete_book_by_bookId
 from cli.inputs.books.index import get_POST_book_input_data, get_PUT_book_input_data
 from cli.console.books.index import print_books_data
+from cli.console.index import print_error, print_warning
 
 def post_book_from_CLI(connection):
     book_data = get_POST_book_input_data()
     post_book(connection, book_data)
 
 def get_all_books_from_CLI(connection):
-    print("List of all books:")
     books = get_all_books(connection)
     if books: print_books_data(books)
-    else:
-        print("No books found.")
+    else: print_warning("No books found.")
 
 def get_book_by_bookId_from_CLI(connection):
     try:
         book_id = int(input("Enter the book ID: "))
         book = get_book_by_bookId(connection, book_id)
         if book: print_books_data([book])
-        else:
-            print("Book not found.")
+        else: print_warning("Book not found.")
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
 
 def put_book_by_bookId_from_CLI(connection):
     try:
         book_id = int(input("Enter the ID of the book to update: "))
         book = get_book_by_bookId(connection, book_id)
         if not book:
-            print("Cannot update it, this book does not exist!\nRepeat the operation and enter an existing ID.")
+            print_warning("Cannot update it, this book does not exist!\nRepeat the operation and enter an existing ID.")
             return
         updated_data = get_PUT_book_input_data()
         put_book_by_bookId(connection, book_id, updated_data)
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
 
 def delete_book_by_bookId_from_CLI(connection):
     try:
         book_id = int(input("Enter the ID of the book to delete: "))
         book = get_book_by_bookId(connection, book_id)
         if not book:
-            print("Cannot delete it, this book does not exist!\nRepeat the operation and enter an existing ID.")
+            print_warning("Cannot delete it, this book does not exist!\nRepeat the operation and enter an existing ID.")
             return
         delete_book_by_bookId(connection, book_id)
     except ValueError:
-        print("Invalid input, ID must be a number.")
+        print_error("Invalid input, ID must be a number.")
