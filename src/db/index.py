8c9671dc1963f2ba716e93df4847400636055541
baketti <e.bachetti1997@gmail.com>
2024-09-18 23:36:14 +0200

    Remove unused imports

--- src/db/index.py
+++ src/db/index.py
@@ -1,6 +1,3 @@
-from sqlalchemy.orm import sessionmaker
-from db.init.index import init_struct
-from cli.console.index import print_success,print_error
 from db.models.DbConnection.index import DbConnection
 
 def create_session():
