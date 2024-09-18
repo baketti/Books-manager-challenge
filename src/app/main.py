4584c0092da8743d784cda2f9d9950020a615a01
baketti <e.bachetti1997@gmail.com>
2024-09-18 12:54:23 +0200

    Add exception handling and HTTPStatus to api responses

--- src/app/main.py
+++ src/app/main.py
@@ -9,39 +9,35 @@ app = Flask(__name__)
 @app.route('/books', methods=['GET'])
 def get_books():
     books = None
-    query_params= request.query_string.decode()
-    if not query_params: 
-        books = get_all_books(db_connection)
-    else:
-        author_name = get_authorName_query_param(query_params)
-        books = get_books_by_authorName(db_connection, author_name)
-        if not books:
+    try:
+        query_params= request.query_string.decode()
+        if not query_params: 
+            books = get_all_books(db_connection)
             return (
-                jsonify({
-                    "message": "No books found"
-                }), 
-                HTTPStatus.NOT_FOUND
-            )
-        
-    return (
-        jsonify(
-            [book.to_dict() for book in books]
-        ),
-        HTTPStatus.OK
-    )
+                jsonify([book.to_dict() for book in books]),
+                HTTPStatus.OK)
+        else:
+            author_name = get_authorName_query_param()
+            books = get_books_by_authorName(db_connection, author_name)
+            if not books:
+                return (
+                    jsonify({
+                        "message": "No books found"
+                    }), 
+                    HTTPStatus.NOT_FOUND)
+        return (
+            jsonify([book.to_dict() for book in books]),
+            HTTPStatus.OK)
+    except Exception as e:
+        return (
+            jsonify({
+                "message": f"An Internal Error occurred during books retrieval: {e}"
+            }),HTTPStatus.INTERNAL_SERVER_ERROR)
 
-def get_authorName_query_param(query_params):
-    params = query_params.split('&')
-    query_items = [{param.split('=')[0]: param.split('=')[1]} for param in params]
-    print(query_items)
-    authorName_item = next(
-        (query_item for query_item in query_items if "authorName" in query_item), 
-        None
-    )
-    author_name = authorName_item.get("authorName").replace("%20", " ")
+def get_authorName_query_param():
+    author_name = request.args.get('authorName')
     sanitized_author_name = ' '.join(author_name.split())
     return sanitized_author_name
-
 
 if __name__ == "__main__":
     app.run()
