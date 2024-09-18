a5e9c03dc0c7e1ce1b64266ae9150466e7f63873
baketti <e.bachetti1997@gmail.com>
2024-09-18 12:55:16 +0200

    Add display_initial_CLI_menu

--- src/cli/index.py
+++ src/cli/index.py
@@ -1,11 +1,14 @@
-from cli.menu.index import main_menu
+from cli.menu.index import initial_cli_menu, main_cli_app_menu
 from cli.operations.index import books_operations, authors_operations
 from rich.prompt import Confirm
 from cli.console.index import print_exit_message
 
-def display_CLI_menu(connection):
+def display_initial_CLI_menu():
+    return initial_cli_menu()
+
+def display_main_CLI_app_menu(connection):
     while True:
-        choice = main_menu()
+        choice = main_cli_app_menu()
         if choice == '1':
             books_operations(connection)
         elif choice == '2':
