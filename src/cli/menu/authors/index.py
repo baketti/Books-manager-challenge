62064df5c2f430bf51ac2765e40bedac5932e13d
baketti <e.bachetti1997@gmail.com>
2024-09-20 00:28:47 +0200

    Update menu

--- src/cli/menu/authors/index.py
+++ src/cli/menu/authors/index.py
@@ -7,7 +7,7 @@ from rich.text import Text
 def authors_menu():
     print_title("AUTHOR MANAGEMENT")
     print_authors_menu()
-    return Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6"])
+    return Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6", "7"])
 
 def print_authors_menu():
     console = Console()   
@@ -19,10 +19,22 @@ def print_authors_menu():
     menu_text.append("3. ", style="magenta")
     menu_text.append("View a specific author\n")
     menu_text.append("4. ", style="magenta")
-    menu_text.append("Update an author\n")
+    menu_text.append("Search author\n")
     menu_text.append("5. ", style="magenta")
-    menu_text.append("Delete an author\n")
+    menu_text.append("Update an author\n")
     menu_text.append("6. ", style="magenta")
+    menu_text.append("Delete an author\n")
+    menu_text.append("7. ", style="magenta")
     menu_text.append("Return to main menu", style="bold magenta")
+    menu_panel = Panel.fit(menu_text, style="bold", border_style="bright_blue")
+    console.print(menu_panel)
+
+def print_search_authors_menu():
+    console = Console()
+    menu_text = Text()
+    menu_text.append("1. ",style="magenta")
+    menu_text.append("Search by name\n")
+    menu_text.append("2. ",style="magenta")
+    menu_text.append("Return to authors menu", style="bold magenta")
     menu_panel = Panel.fit(menu_text, style="bold", border_style="bright_blue")
     console.print(menu_panel)
--- src/cli/menu/books/index.py
+++ src/cli/menu/books/index.py
@@ -7,7 +7,7 @@ from rich.text import Text
 def books_menu():
     print_title("BOOK MANAGEMENT")
     print_books_menu()
-    return Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6"])
+    return Prompt.ask("Select an option", choices=["1", "2", "3", "4", "5", "6", "7"])
 
 def print_books_menu():
     console = Console()
@@ -19,10 +19,24 @@ def print_books_menu():
     menu_text.append("3. ",style="magenta")
     menu_text.append("View a specific book\n")
     menu_text.append("4. ",style="magenta")
-    menu_text.append("Update a book\n")
+    menu_text.append("Search books\n")
     menu_text.append("5. ",style="magenta")
-    menu_text.append("Delete a book\n")
+    menu_text.append("Update a book\n")
     menu_text.append("6. ",style="magenta")
+    menu_text.append("Delete a book\n")
+    menu_text.append("7. ",style="magenta")
     menu_text.append("Return to main menu", style="bold magenta")
+    menu_panel = Panel.fit(menu_text, style="bold", border_style="bright_blue")
+    console.print(menu_panel)
+
+def print_search_books_menu():
+    console = Console()
+    menu_text = Text()
+    menu_text.append("1. ",style="magenta")
+    menu_text.append("Search by title\n")
+    menu_text.append("2. ",style="magenta")
+    menu_text.append("Search by author name\n")
+    menu_text.append("3. ",style="magenta")
+    menu_text.append("Return to books menu", style="bold magenta")
     menu_panel = Panel.fit(menu_text, style="bold", border_style="bright_blue")
     console.print(menu_panel)
