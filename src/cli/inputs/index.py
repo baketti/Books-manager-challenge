b744fa70050fc32525f857c3ffac5d5b0d73e00b
baketti <e.bachetti1997@gmail.com>
2024-09-20 14:15:05 +0200

    Add print_error on validation failure

--- src/cli/inputs/index.py
+++ src/cli/inputs/index.py
@@ -1,3 +1,5 @@
+from cli.console.index import print_error
+
 def get_validated_input(prompt: str, validation_func, is_required: bool=False):
     while True:
         value = input(f"{prompt}: ")
@@ -7,4 +9,4 @@ def get_validated_input(prompt: str, validation_func, is_required: bool=False):
             _value = validation_func(value)
             return _value
         except ValueError as e:
-            print(f"Invalid input: {e}. Please try again!")
+            print_error(f"Invalid input: {e}. Please try again!")
