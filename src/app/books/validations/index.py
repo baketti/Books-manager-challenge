96eb969c3fb13e06451c77c76c3678fccd0b5991
baketti <e.bachetti1997@gmail.com>
2024-09-20 14:54:21 +0200

    Remove author_name from put book data validation

--- src/app/books/validations/index.py
+++ src/app/books/validations/index.py
@@ -1,5 +1,3 @@
-from http import HTTPStatus
-from flask import jsonify
 from services.validation.books.index import validate_title, validate_pages, validate_price, validate_publisher
 from services.validation.authors.index import validate_author_name
 
@@ -23,13 +21,11 @@ def validate_post_book_data(book_data):
 def validate_put_book_data(book_to_update, book_data):
     try:
         title = validate_title(book_data.get('title', book_to_update["title"]))
-        author_name = validate_author_name(book_data.get('author_name', book_to_update["author_name"]))
         pages = validate_pages(book_data.get('pages', book_to_update["pages"]))
         price = validate_price(book_data.get('price', book_to_update["price"]))
         publisher = validate_publisher(book_data.get('publisher', book_to_update["publisher"]))
         return {
             "title": title,
-            "author_name": author_name,
             "pages": pages,
             "price": price,
             "publisher": publisher
