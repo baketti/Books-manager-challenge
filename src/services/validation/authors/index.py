948e5151633a384aac9664ead0b5eaf3d879f108
baketti <e.bachetti1997@gmail.com>
2024-09-20 18:40:49 +0200

    Add birth date and email validation checks useful for apis and fix exception handling

--- src/services/validation/authors/index.py
+++ src/services/validation/authors/index.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 from email_validator import validate_email, EmailNotValidError
 from utils.index import sanitize_string
 
@@ -12,18 +12,35 @@ def validate_author_name(_name: str):
         raise ValueError("Author name must be at most 32 characters long")
     else: return name
 
-def validate_author_birth_date(date: str):
+def validate_author_birth_date(birth_date: str|date):
+    if not birth_date:
+        #works only for apis
+        return None
+    elif isinstance(birth_date, date):
+        #works only for apis
+        return birth_date
+
     try:
-        birth_date = datetime.strptime(date, "%Y/%m/%d")
+        birth_date = datetime.strptime(birth_date, "%Y/%m/%d")
         return birth_date
     except ValueError:
-        raise ValueError("Date format must be YYYY/MM/DD")
+        raise ValueError("date format must be YYYY/MM/DD")
+    except TypeError:
+        raise ValueError("date must be a string")
 
 def validate_author_email(email: str):
+    if not email:
+        #works only for apis
+        return None
+    elif type(email) != str:
+        #works only for apis
+        raise ValueError("email must be a string")
     try:
         validated_email = validate_email(email)
         _email = validated_email["email"]
         return _email
+    except ValueError as e:
+        raise ValueError(e)
     except EmailNotValidError as e:
-        raise ValueError(e)       
+        raise ValueError(f"invalid email ({e})")       
  
