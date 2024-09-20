ff4d98b62c4a1569af11ff7c78995c3ebb6c62d3
baketti <e.bachetti1997@gmail.com>
2024-09-20 16:38:30 +0200

    Moving into routes folder

--- src/app/routes/books/index.py
+++ src/app/routes/books/index.py
@@ -1,14 +1,35 @@
-from services.books.index import get_all_books
+from flask import Blueprint
+from app.middleware.index import validate_post_request_data
 from db.models.DbConnection.index import DbConnection
-from flask import Blueprint, jsonify, request
+from app.routes.books.handlers.post.index import post_books_handler
+from app.routes.books.handlers.put.index import put_book_by_id_handler
+from app.routes.books.handlers.get.index import get_book_by_id_handler, get_books_handler
+from app.routes.books.handlers.delete.index import delete_book_by_id_handler
 
-db_connection = DbConnection().get_connection()
-books_bp = Blueprint('books', __name__)
+db_connection = DbConnection.get_connection()
+books = Blueprint('books', __name__)
 
-# TODO
-@books_bp.route('/books', methods=['GET'])
+@books.before_request
+def check_request_data():
+    return validate_post_request_data()
+
+@books.route('/books', methods=['POST'])
+def post_books():
+    return post_books_handler(db_connection)
+
+@books.route('/books', methods=['GET'])
 def get_books():
-    return (
-        jsonify({"message": "Not implemented yet"}), 
-        501
-    )
+    print("Get books")
+    return get_books_handler(db_connection)
+
+@books.route('/books/<book_id>', methods=['GET'])
+def get_book_by_id(book_id):
+    return get_book_by_id_handler(db_connection,book_id)
+
+@books.route('/books/<book_id>', methods=['PUT'])
+def put_book_by_id(book_id):
+    return put_book_by_id_handler(db_connection, book_id)
+
+@books.route('/books/<book_id>', methods=['DELETE'])
+def delete_book_by_id(book_id):
+    return delete_book_by_id_handler(db_connection, book_id)
