354f34a8bbd8751c562cb936fc0f7da0a8b116d5
baketti <e.bachetti1997@gmail.com>
2024-09-22 16:16:12 +0200

    Add get_book_category

--- src/cli/inputs/books/index.py
+++ src/cli/inputs/books/index.py
@@ -1,6 +1,7 @@
 from cli.inputs.index import get_validated_input
-from services.validation.books.index import validate_title, validate_pages, validate_price, validate_publisher, validate_category
+from services.validation.books.index import validate_title, validate_pages, validate_price, validate_publisher
 from services.validation.authors.index import validate_author_name
+from cli.menu.index import prompt_list_choice
 
 def get_POST_book_input_data():
     title = get_validated_input("Title (required)", validate_title, is_required=True)
@@ -8,7 +9,7 @@ def get_POST_book_input_data():
     pages = get_validated_input("Number of pages (optional)", validate_pages)
     price = get_validated_input("Cover price (optional)", validate_price)
     publisher = get_validated_input("Publisher (optional)", validate_publisher)
-    category = get_validated_input("Category (optional)", validate_category)
+    category = get_book_category()
     return { 
         "title": title,
         "author_name": author_name,
@@ -23,7 +24,7 @@ def get_PUT_book_input_data():
     pages = get_validated_input("New number of pages", validate_pages)
     price = get_validated_input("New cover price", validate_price)
     publisher = get_validated_input("New publisher", validate_publisher)
-    category = get_validated_input("New category", validate_category)
+    category = get_book_category()
     return {
         "title": title,
         "pages": pages,
@@ -31,3 +32,15 @@ def get_PUT_book_input_data():
         "publisher": publisher,
         "category": category
     }
+
+def get_book_category():
+    message = "Select a book category"
+    choices = [
+        {"name": "Classic", "value": "Classic"},
+        {"name": "Drama", "value": "Drama"},
+        {"name": "Fantasy", "value": "Fantasy"},
+        {"name": "IT", "value": "IT"},
+        {"name": "Thriller", "value": "Thriller"},
+        {"name": "Miscellaneous", "value": "Miscellaneous"}
+    ] 
+    return prompt_list_choice(choices, message)
