385bc201f13153e10c0cdd1bb73a7158c02f95a7
baketti <e.bachetti1997@gmail.com>
2024-09-22 08:41:11 +0200

    Invoke DbConnection constructor and remove connection argument

--- src/main.py
+++ src/main.py
@@ -1,26 +1,24 @@
 from cli.console.index import print_exit_message, print_API_start_app
-from db.index import create_session
 from cli.index import display_main_CLI_app_menu, display_initial_CLI_menu
 from app.index import app
 from data.index import import_data_from_csv
 from rich.prompt import Confirm
+from db.models.DbConnection.index import DbConnection 
 
 def main():
     try:
-        connection = create_session()
-
+        DbConnection()
         need_to_import_data = Confirm.ask("\nDo you want to import data from csv?", default=False)
         if need_to_import_data:
-            import_data_from_csv(connection)
-
+            import_data_from_csv()
         choice = display_initial_CLI_menu()   
         if choice == "1":
             # CLI app
-            display_main_CLI_app_menu(connection)
+            display_main_CLI_app_menu()
         else:
             # API app
             print_API_start_app()
-            app.run()
+            app.run(debug=True)
     except KeyboardInterrupt:
         print_exit_message()
         exit(0)  
