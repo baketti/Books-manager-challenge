a327e2391ebd1cf162d74b044b243926a29fe98f
baketti <e.bachetti1997@gmail.com>
2024-09-23 15:40:24 +0200

    Update utilitie functions to solve issues

--- src/utils/index.py
+++ src/utils/index.py
@@ -1,13 +1,13 @@
 def convert_to_number(value,field_name,field_type):
-    if not value or not value.strip():
-        return None 
+    if not value.strip(): return None
     try:
         return field_type(value)
     except ValueError:
         raise ValueError(f"{field_name} must be a number")
 
 def convert_to_dict_list(elements):
-    return [element.to_dict() for element in elements]
+    if not elements: return []
+    return [element.to_dict() for element in elements] if elements else []
 
 def is_updated(updated_data):
     return any(updated_data[key] != None for key in updated_data.keys())
