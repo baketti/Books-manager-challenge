5c93cf65389f24c941d997e540a4461e25193981
baketti <e.bachetti1997@gmail.com>
2024-09-23 12:26:42 +0200

    Update with create_app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from cli.console.index import print_exit_message, print_API_start_app
 from cli.index import display_main_CLI_app_menu, display_initial_CLI_menu
-from app.index import app
+from app.index import create_app
 from data.index import import_data_from_csv
 from rich.prompt import Confirm
 from db.models.DbConnection.index import DbConnection 
@@ -18,10 +18,15 @@ def main():
         else:
             # API app
             print_API_start_app()
+            app = create_app()
             app.run()
     except KeyboardInterrupt:
         print_exit_message()
         exit(0)  
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print_exit_message()
+        exit(1)
        
 if __name__ == '__main__':
     main()
