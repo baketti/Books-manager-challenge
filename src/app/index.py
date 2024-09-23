538b9806c6db71aba0e6a864d0f2a4c698305a0f
baketti <e.bachetti1997@gmail.com>
2024-09-23 12:26:07 +0200

    Wrap app in a function

--- src/app/index.py
+++ src/app/index.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from app.endpoints.books.index import books
 from app.endpoints.authors.index import authors
+from cli.console.index import print_exit_message
 
-app = Flask(__name__)
-app.register_blueprint(books)
-app.register_blueprint(authors)
+def create_app():
+    try:
+        app = Flask(__name__)
+        app.register_blueprint(books)
+        app.register_blueprint(authors)
+        return app
+    except KeyboardInterrupt:
+        print_exit_message()
+        exit(0)
