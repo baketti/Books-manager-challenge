35d1dd5b42632d85731ca1235fb85fb6517317d5
baketti <e.bachetti1997@gmail.com>
2024-09-23 11:58:48 +0200

    Add handle_Integrity_Error utility function

--- src/app/utils/index.py
+++ src/app/utils/index.py
@@ -1,4 +1,5 @@
-from flask import request
+from http import HTTPStatus
+from flask import jsonify, request
 from utils.index import sanitize_string
 
 def get_limit_query_param():
@@ -10,3 +11,17 @@ def get_limit_query_param():
         return int(limit)
     except ValueError:
         raise ValueError("invalid limit value. Limit must be an integer")
+    
+def handle_Integrity_Error(msg: str):
+    # This is to return a correct response message.
+    # Unique constraint errors are handled in the services, but for a specific reason, they aren't raised.
+    # (the reason is that I don't like error messages when data imported from csv already exist...)
+    # Because of this, IntegrityError can't be handled directly.
+    # Therefore, we handle them by returning a correct response message.
+    # When the request reaches the endpoint and the post or put service is called, 
+    # if the item is None, it is only because it already exists,
+    # since the data is validated before reaching this point.
+    return (
+        jsonify({
+            "message": msg
+        }), HTTPStatus.CONFLICT)
